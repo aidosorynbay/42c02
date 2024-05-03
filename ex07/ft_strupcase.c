16060d71de9b1534ddda7b286915dfb7f6bcc9b7
Aidos Orynbay <aorynbay@lab1r5s14.42abudhabi.ae>
2024-05-03 18:39:57 +0400

    ex07

--- ex07/ft_strupcase.c
+++ ex07/ft_strupcase.c
@@ -17,7 +17,10 @@ char	ft_strupcase(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		str[i] = str[i] - 32;
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
 		i++;
 	}
 	return (*str);
