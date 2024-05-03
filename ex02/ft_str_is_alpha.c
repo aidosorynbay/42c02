c61b993c237fc1d0116510beba6feaafddf2715d
Aidos Orynbay <aorynbay@lab1r5s14.42abudhabi.ae>
2024-05-03 16:40:37 +0400

    please, last

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -15,28 +15,29 @@ int	ft_str_is_alpha(char *str)
 	int	i;
 
 	i = 0;
-	if (str[i] >= 65 && str[i] <= 122)
+	while (str[i] >= 65 && str[i] <= 122)
 	{
-		while (str[i] >= 65 && str[i] <= 122)
-		{
-			i++;
-		}
+		i++;
+	}
+	if (str[i] == '\0')
+	{
+		printf("%i\n", 1);
 		return (1);
 	}
 	else
 	{
+		printf("%i\n", 0);
 		return (0);
 	}
 }
 
 // #include <unistd.h>
 // #include <stdio.h>
-
 // int	main(void)
 // {
 // 	char alph_abet[] = "kfjgTvldkijg";
-// 	char not_alph[] = "3543gF/*";
-// 	char empty_str[] = "";
+// 	char not_alph[] = "abcdefghijk2l";
+// 	char empty_str[] = "23adsaffq";
 // 	ft_str_is_alpha(alph_abet);
 // 	ft_str_is_alpha(not_alph);
 // 	ft_str_is_alpha(empty_str);
