983327d78a193e38d8772922ca0fb8cfaf402c34
Aidos Orynbay <aorynbay@lab1r5s14.42abudhabi.ae>
2024-05-03 16:45:48 +0400

    FIRST TRY??

---
+++ ex03/ft_str_is_numeric.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aorynbay <aorynbay@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/03 16:42:08 by aorynbay          #+#    #+#             */
+/*   Updated: 2024/05/03 16:42:15 by aorynbay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		i++;
+	}
+	if (str[i] == '\0')
+	{
+		printf("%i\n", 1);
+		return (1);
+	}
+	else
+	{
+		printf("%i\n", 0);
+		return (0);
+	}
+}
+
+// #include <unistd.h>
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	char alph_abet[] = "384759253";
+// 	char not_alph[] = "48504590g90";
+// 	char empty_str[] = "45kfjgldjg";
+// 	ft_str_is_numeric(alph_abet);
+// 	ft_str_is_numeric(not_alph);
+// 	ft_str_is_numeric(empty_str);
+// }
