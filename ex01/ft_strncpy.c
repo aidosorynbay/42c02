9ffb3d16f422a85b133f8d4a256a64c649cc890d
Aidos Orynbay <aorynbay@lab1r5s14.42abudhabi.ae>
2024-05-03 15:02:44 +0400

    ex02_FINAL

---
+++ ex01/ft_strncpy.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aorynbay <aorynbay@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/03 14:27:01 by aorynbay          #+#    #+#             */
+/*   Updated: 2024/05/03 14:27:03 by aorynbay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	while (i != n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
+}
+
+// #include <unistd.h>
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	char str1[] = "Hello world";
+// 	char str2[] = "Goodbye";
+// 	ft_strncpy(str1, str2, 3); 
+// 	printf("%s", str1);
+// }
